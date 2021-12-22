a03d57c4c5b7eba7771f10022e8e3088bba40389
listyantidewi1 <dewi@smkn12malang.sch.id>
2021-12-22 11:05:49 +0700

    22/12/2021

---
+++ insert.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Insert Data</title>
+</head>
+
+<body>
+    <?php
+    require_once "connect.php"; //mutlak
+
+    $query2 = "insert into provinces values (96, 'NEW JERSEY')";
+
+    if ($konek->query($query2) === TRUE) {
+        echo "data berhasil ditambahkan";
+    } else {
+        echo "Error: " . $sql . "<br>" . $konek->error;
+    }
+
+    $konek->close();
+    ?>
+</body>
+
+</html>
