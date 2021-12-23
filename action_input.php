74bc084996e8b3b5e70830cfa897cb752f796284
listyantidewi1 <dewi@smkn12malang.sch.id>
2021-12-23 09:05:45 +0700

    23/12/2021

---
+++ action_input.php
@@ -0,0 +1,7 @@
+<?php
+$id_provinsi = $_POST['id'];
+$nama_provinsi = $_POST['name'];
+
+echo $id_provinsi;
+echo "<br />";
+echo $nama_provinsi;
---
+++ form_input.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Input Data Melalui Form</title>
+</head>
+
+<body>
+    <form action="action_input.php" method="POST">
+        <label for="id">Id Provinsi:</label><br>
+        <input type="text" id="id" name="id"><br>
+        <label for="name">Nama Provinsi:</label><br>
+        <input type="text" id="name" name="name"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
